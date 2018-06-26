e35e98e680fec2fa9910b73d41b69d959b81e49c
Monami Tsunawaki <monami.tsunawaki@gmail.com>
2018-06-26 13:57:11 +0900

    first repository

---
+++ STEP5.py
@@ -0,0 +1,27 @@
+import csv
+import itertools
+import math
+#２点間の距離を求める関数dest
+def dest(list1, list2):
+    l = list(map(lambda x,y:(x-y)**2, list1, list2))
+    s = math.sqrt(sum(l))
+    return s
+
+with open('input_0.csv','r') as f:
+    dataReader = csv.reader(f)
+    point = []
+    #csvファイルから点の座標を取り出しリストpointに入れる
+    for row in dataReader:
+        point.append(row)
+    del point[0]
+    floatpoint = []
+    for p in point:
+        floatp = list(map(float, p))
+        floatpoint.append(floatp)
+    #全ての並べ方をallに列挙
+    all = list(itertools.permutations(floatpoint[0:5]))
+    tekitou = []
+    for a in all:
+        destination = dest(a[0], a[1]) + dest(a[1], a[2]) + dest(a[2], a[3]) +dest(a[3],a[4]) + dest(a[4],a[0])
+        tekitou.append(destination)
+    print(min(tekitou))
